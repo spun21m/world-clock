00b426939fb0f47d98217698f2a102c18d088d86
[Sital Pun] <[shitalmagar21@gmail.com]>
2023-04-09 03:20:57 -0400

    Display time of the selected location

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,15 @@
 function updateTime(timezone) {
   let city = timezone.split("/")[1].toLowerCase();
   let cityElement = document.querySelector(`#${city}`);
-  let cityDate = cityElement.querySelector(".date");
-  cityDate.innerHTML = moment().tz(`${timezone}`).format("MMMM Do YYYY");
+  if (cityElement) {
+    let cityDate = cityElement.querySelector(".date");
+    cityDate.innerHTML = moment().tz(`${timezone}`).format("MMMM Do YYYY");
 
-  let cityTime = cityElement.querySelector(".time");
-  cityTime.innerHTML = moment()
-    .tz(`${timezone}`)
-    .format("h:mm:ss [<small>] A [</small>]");
+    let cityTime = cityElement.querySelector(".time");
+    cityTime.innerHTML = moment()
+      .tz(`${timezone}`)
+      .format("h:mm:ss [<small>] A [</small>]");
+  }
 }
 
 updateTime("America/Los_Angeles");
@@ -21,3 +23,26 @@ let intervalForCairo = "Africa/Cairo";
 setInterval(updateTime, 1000, intervalForLosAngeles);
 setInterval(updateTime, 1000, intervalForMontreal);
 setInterval(updateTime, 1000, intervalForCairo);
+
+function updateCityTime(event) {
+  let selectedTimeZone = event.target.value;
+  let cityName = selectedTimeZone.replace("_", " ").split("/")[1];
+  let selectedCityDate = moment()
+    .tz(`${selectedTimeZone}`)
+    .format("MMMM Do YYYY");
+  let selectedCityTime = moment()
+    .tz(`${selectedTimeZone}`)
+    .format("h:mm:ss [<small>] A [</small>]");
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+          <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${selectedCityDate}</div>
+          </div>
+          <div class="time">${selectedCityTime}</div>
+        </div>`;
+}
+
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", updateCityTime);
